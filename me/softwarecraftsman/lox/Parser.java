fb3d71d32503a9aef5784d2cbef61359947d979c
Adil Khan <adil.khan@outlook.com>
2022-02-08 16:10:10 -0500

    Adding statments

--- me/softwarecraftsman/lox/Parser.java
+++ me/softwarecraftsman/lox/Parser.java
@@ -1,4 +1,5 @@
 package me.softwarecraftsman.lox;
+import java.util.ArrayList;
 import java.util.List;
 
 import static me.softwarecraftsman.lox.TokenType.*;
@@ -13,16 +14,22 @@ public class Parser {
         this.tokens = tokens;
     }
 
-    Expr parse() {
-        try {
-            return expression();
-        } catch(ParseError error) {
-            return null;
+    List<Stmt> parse() {
+        List<Stmt> statements = new ArrayList<>();
+        while(!isAtEnd()){
+            statements.add(statement());
         }
+        return statements;
     }
 
     private Expr expression() {
         return equality();
+    }
+
+    private Stmt statement() {
+        if(match(PRINT)) return printStatement();
+
+        return expressionStatement();
     }
 
     private Expr equality() {
